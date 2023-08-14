c5a36811f3d4de96ff49336dd6a9dfa9cf01ee4a
Finomnis <finomnis@gmail.com>
2023-08-14 08:06:46 +0200

    Remove obsolete comment

--- src/hid_descriptor.rs
+++ src/hid_descriptor.rs
@@ -1,7 +1,5 @@
 use usbd_hid::descriptor::generator_prelude::*;
 
-/// MouseReport describes a report and its companion descriptor than can be used
-/// to send mouse movements and button presses to a host.
 #[gen_hid_descriptor(
     (collection = APPLICATION, usage_page = VENDOR_DEFINED_START, usage = 0x0100) = {
         (usage = 0x02,) = {
