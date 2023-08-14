f9311cdc0775e29d0ac9eb72f077dcd71600e2dd
Finomnis <finomnis@gmail.com>
2023-08-14 08:12:33 +0200

    Convert example to rtic

--- examples/teensy_micromod.rs
+++ examples/teensy_micromod.rs
@@ -5,9 +5,8 @@
 #![no_std]
 #![no_main]
 
-mod common;
-
 use teensy4_bsp as bsp;
+use teensy4_panic as _;
 
 #[rtic::app(device = teensy4_bsp)]
 mod app {
@@ -15,12 +14,12 @@ mod app {
     use bsp::board;
     use bsp::hal;
 
+    use embedded_hal::serial::Write;
+
     use hal::usbd::{BusAdapter, EndpointMemory, EndpointState};
     use usb_device::bus::UsbBusAllocator;
 
     use teensy4_selfrebootor::Rebootor;
-
-    use crate::common::uart::{uart_log, UartWriter};
 
     /// This allocation is shared across all USB endpoints. It needs to be large
     /// enough to hold the maximum packet size for *all* endpoints. If you start
@@ -30,8 +29,13 @@ mod app {
     /// of endpoints; we're not using all the endpoints in this example.
     static EP_STATE: EndpointState = EndpointState::max_endpoints();
 
+    const LOG_POLL_INTERVAL: u32 = board::PERCLK_FREQUENCY / 100;
+    const LOG_DMA_CHANNEL: usize = 0;
+
     #[local]
     struct Local {
+        poll_log: hal::pit::Pit<3>,
+        log_poller: bsp::logging::Poller,
         rebootor: Rebootor<'static>,
         led: board::Led,
     }
@@ -42,6 +46,8 @@ mod app {
     #[init(local = [bus: Option<UsbBusAllocator<BusAdapter>> = None])]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         let board::Resources {
+            mut dma,
+            pit: (_, _, _, mut poll_log),
             pins,
             usb,
             mut gpio2,
@@ -49,16 +55,18 @@ mod app {
             ..
         } = board::tmm(cx.device);
 
-        // Initialize UART
-        let mut uart = UartWriter::new(board::lpuart(lpuart6, pins.p1, pins.p0, 115200));
-        writeln!(uart);
-
-        // Write welcome message
-        writeln!(uart, "===== Rebootor example =====");
-        writeln!(uart);
-
-        // Initialize logging
-        uart_log::init(uart, log::LevelFilter::Debug);
+        // Logging
+        let log_dma = dma[LOG_DMA_CHANNEL].take().unwrap();
+        let mut log_uart = board::lpuart(lpuart6, pins.p1, pins.p0, 115200);
+        for &ch in "\r\n===== Rebootor example =====\r\n\r\n".as_bytes() {
+            nb::block!(log_uart.write(ch)).unwrap();
+        }
+        nb::block!(log_uart.flush()).unwrap();
+        let log_poller =
+            imxrt_log::log::lpuart(log_uart, log_dma, imxrt_log::Interrupts::Enabled).unwrap();
+        poll_log.set_interrupt_enable(true);
+        poll_log.set_load_timer_value(LOG_POLL_INTERVAL);
+        poll_log.enable();
 
         // Initialize LED
         let led = board::led(&mut gpio2, pins.p13);
@@ -70,14 +78,36 @@ mod app {
         let bus = cx.local.bus.insert(UsbBusAllocator::new(bus));
         let rebootor = teensy4_selfrebootor::Rebootor::new(bus);
 
-        (Shared {}, Local { rebootor, led }, init::Monotonics())
+        (
+            Shared {},
+            Local {
+                log_poller,
+                poll_log,
+                rebootor,
+                led,
+            },
+            init::Monotonics(),
+        )
     }
 
-    #[task(binds = USB_OTG1, local = [rebootor, led], priority = 2)]
+    #[task(binds = USB_OTG1, local = [rebootor, led], priority = 5)]
     fn usb1(ctx: usb1::Context) {
         let usb1::LocalResources { rebootor, led } = ctx.local;
 
         rebootor.poll();
         led.toggle();
+    }
+
+    #[task(binds = PIT, priority = 1, local = [poll_log, log_poller])]
+    fn blink_and_log(cx: blink_and_log::Context) {
+        let blink_and_log::LocalResources {
+            poll_log,
+            log_poller,
+        } = cx.local;
+
+        if poll_log.is_elapsed() {
+            poll_log.clear_elapsed();
+            log_poller.poll();
+        }
     }
 }
